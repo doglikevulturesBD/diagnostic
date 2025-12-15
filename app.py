f911788fd737a1d940b5b4cd3364a67db65e0f4a
doglikevulturesBD <doglikevultures@gmail.com>
2025-12-15 17:35:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from pathlib import Path
 import streamlit as st
 
 # -------------------------------------------------
+# Load custom CSS
+# -------------------------------------------------
+def load_css(file_name):
+    css_path = ROOT / file_name
+    with open(css_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("style.css")
+
+
+# -------------------------------------------------
 # Ensure local imports always work (Streamlit-safe)
 # -------------------------------------------------
 ROOT = Path(__file__).resolve().parent
